29c89d603265e2833c806da5ac0b83bfc8835432
Nate Laptop <parkerjn3@gmail.com>
2016-10-06 21:38:53 -0400

    Style sheet changes

--- Nathan/Controllers/ContactController.cs
+++ Nathan/Controllers/ContactController.cs
@@ -1,40 +1,43 @@
 ﻿namespace Nathan.Controllers
 {
-    using System.Web.Mvc;
-    using Nathan.Helpers;
-    using ViewModels;
+	using Helpers;
+	using System.Web.Mvc;
+	using ViewModels;
 
-    public class ContactController : BaseController
-    {
-        public ActionResult Index()
-        {
-            var viewModel = new ContactViewModel();
-            return View(viewModel);
-        }
+	public class ContactController : BaseController
+	{
+		public ActionResult Index()
+		{
+			var viewModel = new ContactViewModel()
+			{
+				CurrentTab = "Contact"
+			};
+			return View(viewModel);
+		}
 
-        [HttpPost]
-        public ActionResult Index(ContactViewModel submission)
-        {
-            if (ModelState.IsValid)
-            {
-                Emailer.SendMsg(
-                    submission.Email, // From email
-                    submission.Name, // From name
-                    submission.Subject, // Subject
-                    submission.Message, // Email body
-                    null, // cc
-                    null); // bcc
-                return View("Confirm");
-            }
+		[HttpPost]
+		public ActionResult Index(ContactViewModel submission)
+		{
+			if (ModelState.IsValid)
+			{
+				Emailer.SendMsg(
+					submission.Email, // From email
+					submission.Name, // From name
+					submission.Subject, // Subject
+					submission.Message, // Email body
+					null, // cc
+					null); // bcc
+				return View("Confirm");
+			}
 
-            var viewModel = new ContactViewModel();
-            return View(viewModel);
-        }
+			var viewModel = new ContactViewModel();
+			return View(viewModel);
+		}
 
-        public ActionResult Confirm()
-        {
-            var viewModel = new ContactViewModel();
-            return View("Confirm");
-        }
-    }
+		public ActionResult Confirm()
+		{
+			var viewModel = new ContactViewModel();
+			return View("Confirm");
+		}
+	}
 }
--- Nathan/Controllers/SkillsController.cs
+++ Nathan/Controllers/SkillsController.cs
@@ -1,14 +1,17 @@
 ﻿namespace Nathan.Controllers
 {
-    using System.Web.Mvc;
-    using ViewModels;
+	using System.Web.Mvc;
+	using ViewModels;
 
-    public class SkillsController : BaseController
-    {
-        public ActionResult Index()
-        {
-            var viewModel = new BaseViewModel();
-            return View(viewModel);
-        }
-    }
+	public class SkillsController : BaseController
+	{
+		public ActionResult Index()
+		{
+			var viewModel = new BaseViewModel()
+			{
+				CurrentTab = "Skills"
+			};
+			return View(viewModel);
+		}
+	}
 }
